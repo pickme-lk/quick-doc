8a01853887b00036baf070be2a6879758c471ffc
jagath <jagath.bandara@pickme.lk>
2023-04-17 12:46:26 +0530

    Fixed the issues in showing the slices and the circular referances in structs. Child nods will stop recurring if there is a reference to the same type of struct in the hierarchy

--- schema/schema.go
+++ schema/schema.go
@@ -36,22 +36,23 @@ func (b *Builder) GetSchema(obj interface{}) (*Property, error) {
 	}
 	t := reflect.TypeOf(obj)
 	v := reflect.ValueOf(obj)
-
-	return b.inspect(t, v)
+	visited := make(map[string][][]int)
+	currentPath := []int{1}
+	return b.inspect(t, v, visited, currentPath)
 }
 
-func (b *Builder) inspect(t reflect.Type, v reflect.Value) (*Property, error) {
+func (b *Builder) inspect(t reflect.Type, v reflect.Value, visited map[string][][]int, currentPath []int) (*Property, error) {
 	switch t.Kind() {
 	case reflect.Interface:
 		if v.Interface() == nil {
 			return nil, nil
 		}
-		return b.inspect(v.Elem().Type(), v.Elem())
+		return b.inspect(v.Elem().Type(), v.Elem(), visited, currentPath)
 	case reflect.Ptr:
 		if !v.IsValid() {
-			return b.inspect(t.Elem(), reflect.Value{})
+			return b.inspect(t.Elem(), reflect.Value{}, visited, currentPath)
 		}
-		return b.inspect(t.Elem(), v.Elem())
+		return b.inspect(t.Elem(), v.Elem(), visited, currentPath)
 	case reflect.String:
 		return &Property{
 			Type:  PropType_STRING,
@@ -77,7 +78,35 @@ func (b *Builder) inspect(t reflect.Type, v reflect.Value) (*Property, error) {
 		if !v.IsValid() && !b.Options.ExploreNilStruct {
 			return nil, nil
 		}
-		props, err := b.inspectStruct(t, v)
+		structType := t.String()
+		props := make([]Property, 0)
+		var err error
+		visitedPath := (visited)[structType]
+		if visitedPath == nil || len(visitedPath) == 0 {
+			(visited)[structType] = [][]int{currentPath}
+			props, err = b.inspectStruct(t, v, visited, currentPath)
+			if err != nil {
+				return nil, err
+			}
+			return &Property{
+				Type:       PropType_OBJECT,
+				Properties: props,
+			}, nil
+		} else {
+			for _, v := range visitedPath {
+				prefixCandidate := currentPath[0:len(v)]
+				if reflect.DeepEqual(prefixCandidate, v) {
+					//stopping the further exploration when a cyclic dependency is found in the tree branch
+					return &Property{
+						Type:       PropType_OBJECT,
+						Properties: props,
+					}, nil
+				}
+			}
+		}
+		visitedPath = append(visitedPath, currentPath)
+		(visited)[structType] = visitedPath
+		props, err = b.inspectStruct(t, v, visited, currentPath)
 		if err != nil {
 			return nil, err
 		}
@@ -87,15 +116,17 @@ func (b *Builder) inspect(t reflect.Type, v reflect.Value) (*Property, error) {
 		}, nil
 	case reflect.Slice:
 		props := make([]Property, 0)
-		if v.Len() == 0 {
-			prop, err := b.inspect(t.Elem(), reflect.Value{})
+		if !v.IsValid() || v.Len() == 0 {
+			prop, err := b.inspect(t.Elem(), reflect.Value{}, visited, currentPath)
 			if err != nil {
 				return nil, err
 			}
-			props = append(props, *prop)
+			if prop != nil {
+				props = append(props, *prop)
+			}
 		} else {
 			for i := 0; i < v.Len(); i++ {
-				prop, err := b.inspect(t.Elem(), v.Index(i))
+				prop, err := b.inspect(t.Elem(), v.Index(i), visited, currentPath)
 				if err != nil {
 					return nil, err
 				}
@@ -113,9 +144,13 @@ func (b *Builder) inspect(t reflect.Type, v reflect.Value) (*Property, error) {
 	}
 }
 
-func (b *Builder) inspectStruct(t reflect.Type, v reflect.Value) ([]Property, error) {
+func (b *Builder) inspectStruct(t reflect.Type, v reflect.Value, visited map[string][][]int, currentPath []int) ([]Property, error) {
 	props := make([]Property, 0)
 	for i := 0; i < t.NumField(); i++ {
+		nextPath := make([]int, len(currentPath))
+		copy(nextPath, currentPath)
+		nextPath = append(nextPath, i)
+
 		_field := t.Field(i)
 		var _value reflect.Value
 		if v.IsValid() {
@@ -123,7 +158,7 @@ func (b *Builder) inspectStruct(t reflect.Type, v reflect.Value) ([]Property, er
 		} else {
 			_value = reflect.Value{}
 		}
-		prop, err := b.inspect(_field.Type, _value)
+		prop, err := b.inspect(_field.Type, _value, visited, nextPath)
 		if err != nil {
 			return nil, err
 		}
@@ -149,7 +184,7 @@ func (b *Builder) structFieldName(sf reflect.StructField) string {
 
 func (b *Builder) valueString(v reflect.Value) string {
 	if !v.IsValid() {
-		return "nil"
+		return ""
 	}
 	return fmt.Sprintf("%v", v)
 }
